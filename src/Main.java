fc3a823e2c8f93390e8d4dfd5c8f0dee1f80c252
Anna <mysina93@mail.ru>
2026-06-25 11:08:29 +0300

    Исправила ДЗ

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
             System.out.println(i);
         }
         System.out.println("Задача 4");
-        for (int i = -10; i <= 10; i = i + 1) {
+        for ( int i = 10; i >= -10;i = i-1){
             System.out.println(i);
         }
         System.out.println("Задача 5");
@@ -49,7 +49,7 @@ public class Main {
 
         System.out.println("Задача 10");
         int y = 2;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i < 10; i++) {
             System.out.println(y + " * " + i + " = " + (i * y));
         }
     }
